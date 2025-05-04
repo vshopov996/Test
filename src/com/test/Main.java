8207b95820ebee2cd5e78fb77584698e71d4d1b5
vshopov996 <vasownz@abv.bg>
2025-05-04 13:08:12 +0300

    updating code

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,7 +1,10 @@
 package com.test;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hedfdgffgdfgdfgdfglo world!");
+        System.out.println("Hello Git!");
+
+        System.out.println("Now I can continue make changes to my project :)");
     }
 }
